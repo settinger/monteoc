378da43b53321f979713b828577ea16c7726bfac
Sam Ettinger <ettingersam@gmail.com>
2021-04-30 22:47:41 -0700

    Added command-line instruction

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const HelloWorld = () => {
-  return <h3>Hello World</h3>;
-};
+const HelloWorld = () => (
+  <div>
+    <p>
+      <b>monteoc</b> <i>noun</i> (Huave language) lightning. Cf.{" "}
+      <b>ajüy teat monteoc</b>, thunder.
+    </p>
+    <p>
+      Monteoc tajmiüc wüx noic owil xiül, nómban achiüt.
+      <br />
+      <i>The lighting struck a tree and split it.</i>
+    </p>
+    <p>
+      <small>
+        Stairs Kreger, Glenn Albert; Scharfe de Stairs, Emily Florence; Olvaries
+        Oviedo, Proceso; Ponce Villanueva, Tereso; Comonfort Llave, Lorenzo.{" "}
+        <a href="https://www.sil.org/resources/archives/10944">
+          <i>Diccionario huave de San Mateo del Mar</i>
+        </a>{" "}
+        (Serie de vocabularios indígenas “Mariano Silva y Aceves”; 24)‎, México,
+        D.F.: Instituto Lingüístico de Verano, 1981, pages 116, 266
+      </small>
+    </p>
+  </div>
+);
 
 export default HelloWorld;
